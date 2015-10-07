7903e7dfd8db6d3655f59e0ec699db2293d46b1c
Ram Adhikari <ram.adhikari@considerthisu.com>
2015-10-07 14:45:22 +0100

    Adding Script for Google Drop Pin Map

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package: google-map
+ * @FileType: PHP Script
+ * @author RC Adhikari <http://rcadhikari.blogspot.co.uk>
+ * Date: 25/08/2015 15:13
+ */
+
+require_once 'header.php';
+?>
+
+<div class="container">
+    Please click the suitable example as below:
+
+    <ul>
+        <li> <a href="location-marker.php" title="Create Google Marker">Create Google Marker</a> </li>
+        <li> <a href="location-drop-pin-maps.php" title="Create Google Marker">Google Multi Drop Pin Maps</a> </li>
+    </ul>
+</div>
